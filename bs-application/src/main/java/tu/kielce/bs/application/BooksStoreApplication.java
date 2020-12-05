78617077e714ec13b925896a7895248b038784e0
Michal Piotrowski <97.piotrowski.michal@gmail.com>
2020-12-05 11:00:48 +0100

    change packages names

--- bs-application/src/main/java/tu/kielce/bs/application/BooksStoreApplication.java
+++ bs-application/src/main/java/tu/kielce/bs/application/BooksStoreApplication.java
@@ -2,8 +2,13 @@ package tu.kielce.bs.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "tu.kielce.bs.book",
+        "tu.kielce.bs.user"
+})
 public class BooksStoreApplication {
 
     public static void main(String[] args) {
